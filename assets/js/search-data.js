0c4180c20089638ac9cc8f0b4e9bb0f8dbcc0827
Jaber-Valinejad <66491004+Jaber-Valinejad@users.noreply.github.com>
2025-03-27 02:09:54 +0000

    Deploying to gh-pages from @ Jaber-Valinejad/Jaber-Valinejad.github.io@dbeea53c3b84d4810153542a55f3ad6c140c2d44 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -437,9 +437,9 @@ ninja.data = [{
           description: "ETL-Driven Knowledge Graph Construction for Improving Drug Safety and Pharmacovigilance in Rare Diseases",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-energy-market",
+          title: 'Energy market',
+          description: "Multi-Carrier Energy Market Optimization &amp; Bayesian Uncertainty Quantification",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
